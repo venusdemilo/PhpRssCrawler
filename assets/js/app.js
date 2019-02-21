c3dd1bd4c602c8cb2b9cc608a3bc02030a98b6fb
philippe <philippe.lapeyrie@chu-nimes.fr>
2019-02-21 19:42:11 +0100

    Jquery success

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,8 +7,23 @@
 
 // any CSS you require will output into a single css file (app.css in this case)
 require('../css/app.css');
+require ('bootstrap');
+
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
-// const $ = require('jquery');
+ const $ = require('jquery');
 
-console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
+console.log('Hello coco Webpack Encore! Edit me in assets/js/app.js');
+
+
+// or you can include specific pieces
+// require('bootstrap/js/dist/tooltip');
+// require('bootstrap/js/dist/popover');
+/*
+$(document).ready(function() {
+    $('[data-toggle="popover"]').popover();
+});
+*/
+$(document).ready(function() {
+    $('#cible').append('coco');
+});
