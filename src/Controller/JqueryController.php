ce8db4a85dbc6f5fc3edfde3c069b646246c360c
VenusCHU <philippe@lapeyrie.fr>
2019-02-22 12:38:04 +0100

    simplepie test OK

--- src/Controller/JqueryController.php
+++ src/Controller/JqueryController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use SimplePie;
 
 class JqueryController extends AbstractController
 {
@@ -12,6 +13,14 @@ class JqueryController extends AbstractController
      */
     public function index()
     {
+      $urlFeed = 'https://www.lemonde.fr/rss/une.xml';
+      $feed = new SimplePie();
+   $feed->set_cache_location('/home/philippe/SimplePieCache');
+   $feed->set_feed_url($urlFeed);
+   $feed->init();
+   $feed->handle_content_type();
+   //if ($feed->error())
+
         return $this->render('jquery/index.html.twig', [
             'controller_name' => 'JqueryController',
         ]);
